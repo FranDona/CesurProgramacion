32d1e67ebdc648311a363ffdc4826953712d0d07
Fran Dona Villar <149049232+FranDona@users.noreply.github.com>
2026-04-04 22:01:44 +0200

    Add files via upload

---
+++ Programacion_UD6/Estudiante.java
@@ -0,0 +1,12 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double nota;
+
+    //Constructor
+    public Estudiante(String nombre, int edad, double nota) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.edad = nota;
+    }
+}
