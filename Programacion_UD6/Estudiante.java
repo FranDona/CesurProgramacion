71a2c67809cbb338884ea7380e54712fed9a26b6
Fran Dona Villar <149049232+FranDona@users.noreply.github.com>
2026-04-06 14:57:20 +0200

    Add files via upload

--- Programacion_UD6/Estudiante.java
+++ Programacion_UD6/Estudiante.java
@@ -6,7 +6,25 @@ public class Estudiante {
     //Constructor
     public Estudiante(String nombre, int edad, double nota) {
         this.nombre = nombre;
-        this.edad = edad;
-        this.edad = nota;
+        this.edad = edad;   
+        this.nota = nota;
+    }
+
+   //Getters
+    public String getNombre() {
+        return nombre;
+    }    
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Edad: " + edad + ", Nota: " + nota;
     }
 }
--- Programacion_UD6/main.java
+++ Programacion_UD6/main.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
+
+    static Scanner sc = new Scanner(System.in);
+    
+    //Array con 3 estudiantes cargados (fijo)
+    static Estudiante[] estudiantesArray = new Estudiante[3];
+
+    //Array de estudiantes (dinamico)
+    static ArrayList<Estudiante> estudiantesList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+        int opcion;
+
+        do {
+            System.out.println("\n--- MENÚ PRINCIPAL ---");
+            System.out.println("1. Array");
+            System.out.println("2. ArrayList");
+            System.out.println("0. Cerrar");
+            System.out.print("Opcion:");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    menuArray();    
+                    break;
+                case 2:
+                    menuArrayList();    
+                    break;
+            }
+
+        } while (opcion != 0);
+            System.out.println("Cerrado");           
+        }
+    
+        //MENU ARRAY
+
+
+
+        //MENU ARRAYLIST
     
 }
